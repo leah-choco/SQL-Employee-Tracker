3bae72739d2d74a21372f8da74337a30139ab081
Your Name <you@example.com>
2022-10-18 11:56:01 -0500

    Creating tables in the schema

--- db/schema.sql
+++ db/schema.sql
@@ -16,3 +16,12 @@ CREATE TABLE department_role (
     PRIMARY KEY (id)
 );
 
+CREATE TABLE employee (
+    id INT NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    PRIMARY KEY (id)
+);
+
