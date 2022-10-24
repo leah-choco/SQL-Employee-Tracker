2aaf413217900d17c2061593ad35b644d1e74608
Your Name <you@example.com>
2022-10-24 11:56:16 -0500

    Finished adding data for seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,13 +3,22 @@ VALUES
     ("Engineering"),
     ("Sales"),
     ("Finance"),
-    ("Legal");
+    ("Legal"),
+    ("Human Resources");
 
-INSERT INTO roles (title, salary, department_id)    
+INSERT INTO roles(title, salary, department_id)    
 VALUES 
     ("Software Engineer", 120000, 001),
-    ("Account Manager", 160000, 002);
+    ("Account Manager", 160000, 002),
+    ("CFO", 200000, 003),
+    ("Lawyer", 50000, 004),
+    ("Manager", 1000000, 005);
 
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("John", "Doe", 002, 1);
+VALUES 
+    ("Juan", "Choco", 002, 1),
+    ("Leah", "Choco", 001, 1),
+    ("Zeke", "Choco", 004, 1),
+    ("Solomon", "Choco", 003, 1),
+    ("Maya", "Choco", 005, 1);
